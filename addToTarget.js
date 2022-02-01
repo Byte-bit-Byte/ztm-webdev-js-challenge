094402d2270c8830cb04d349beaf7101b7216e9b
Efe <efe@gitbash.pc>
2022-01-31 19:56:30 -0600

    Added comments to addToTarget function

--- addToTarget.js
+++ addToTarget.js
@@ -1,16 +1,25 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+// Create function that accepts the array and target
 const addToTarget = (numArray, target) => {
+	// Create an additive inverse array based on the target numer
 	const addInv = numArray.map(number => target - number);
+
+	// Create a variable to hold the index in the array during the loop
 	let numIndex = 0;
 
+	// Loop through the number array
 	for (item of numArray) {
-		if (addInv.indexOf(item) !== -1) {
-			numIndex = addInv.indexOf(item);
+		// Puts the current index being checked into an object
+		// Avoids calling the function multiple time uncessarily
+		numIndex = addInv.indexOf(item);
+		if (numIndex !== -1) {
+			// Return array containing the first set of two numbers that sum to target
 			return [item, numArray[numIndex]];
 		}
 	}
 }
 
+// Test function
 let test = addToTarget(array, 4);
 test;
